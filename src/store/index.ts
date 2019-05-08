5a6fa2781e9094c34a9e4238318da37722138952
leob <leobdev@gmail.com>
2019-05-08 17:17:05 +0800

    Refactor redux store etc

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,63 @@
 import { StateType } from 'typesafe-actions';
-import { Middleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose, Middleware } from 'redux';
 
-import rootReducer from './root-reducer';
-
-import { fetchScenariosMiddleware } from './scenarios/middleware';
-// import { fetchSessionsMiddleware } from './sessions/middleware';
-
-import * as scenariosSelectors from './scenarios/selectors';
-import * as sessionsSelectors from './sessions/selectors';
-
+// Actions
 import * as scenariosActions from './scenarios/actions';
 import * as sessionsActions from './sessions/actions';
 
-export { default } from './store';
-export { default as rootReducer } from './root-reducer';
+// Selectors
+import * as scenariosSelectors from './scenarios/selectors';
+import * as sessionsSelectors from './sessions/selectors';
 
-export const selectors = {
-  sessions: sessionsSelectors,
-  scenarios: scenariosSelectors
-};
+// Middlewares
+import { fetchScenariosMiddleware } from './scenarios/middleware';
+// import { fetchSessionsMiddleware } from './sessions/middleware';
 
+// Reducers
+import scenarios from './scenarios/reducer';
+import sessions from './sessions/reducer';
+
+// Actions
 export const actions = {
   sessions: sessionsActions,
   scenarios: scenariosActions
 }
 
-export const middlewares: Middleware[] = [
-  fetchScenariosMiddleware
-//   fetchSessionsMiddleware
-]
+// Selectors
+export const selectors = {
+  sessions: sessionsSelectors,
+  scenarios: scenariosSelectors
+};
 
+// Middlewares
+const middlewares: Middleware[] = [
+  fetchScenariosMiddleware
+//   fetchSessionsMiddleware,
+];
+
+// Reducers
+export const rootReducer = combineReducers({
+  scenarios,
+  sessions
+});
+
+// Store
+
+// root state
 export type RootState = StateType<typeof rootReducer>;
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+function configureStore(initialState?: {}) {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
+}
+
+// store; pass an optional param to rehydrate state on app start
+const store = configureStore();
+export default store
+
+
